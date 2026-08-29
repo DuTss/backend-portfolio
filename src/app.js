a53f41182f30d58789aa6741a933c2111ddf0732
Emilien .D <emduthy@gmail.com>
2026-08-29 11:59:01 +0200

    correction du cors avec global content

--- src/app.js
+++ src/app.js
@@ -175,6 +175,14 @@ app.use(cors({
   credentials: true
 }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 // --- Middlewares globaux ---
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
